aab934ea8fc06cdfac8f9c301b6c3c2261a8800c
hitman1717 <rohitkuttumu17@gmail.com>
2026-04-13 23:29:12 +0530

    BUG FIXED

--- src/services/streakService.js
+++ src/services/streakService.js
@@ -1,4 +1,4 @@
-import { collection, query, where, getDocs, Timestamp } from "firebase/firestore";
+import { collection, query, where, getDocs, Timestamp, orderBy, limit } from "firebase/firestore";
 import { db } from "./firebase";
 
 /**
